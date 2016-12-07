8bd65888b4374b9f5ead05a106b47cb87159b158
Joachim Bruno Strudsholm <joac0249@edu.easj.dk>
2016-12-07 12:48:04 +0100

    funktioner done

--- Access.php
+++ Access.php
@@ -1,10 +1,62 @@
 <?php
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_URL, 'http://localhost:2727/WcfService.svc/GetAccessList/');
+$result = curl_exec($curl);
+curl_close($curl);
+?>
+<?php
+if ($_POST["d"] != ""){
+    echo ' asgdhaslsakdfklasdflkjasn';
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'http://localhost:2727/WcfService.svc/AddDoorAccess/');
+    curl_setopt($curl, CURLOPT_POST, true);
+    $data = array('CardId' => $_POST["c"], 'DoorId' => $_POST['d']);
+    $jsonData = json_encode($data);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $headers = [
+        'Host: localhost:2727',
+        'Content-Type: application/json'
+    ];
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    $postResult = curl_exec($curl);
+    curl_close($curl);
+
+
+}
+?>
+<?php
+if ($_POST["AccessDelete"] != "") {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'http://localhost:2727/WcfService.svc/DeleteDoorAccess/');
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+    $data = array('CardId' => $_POST["AccessDelete"]);
+    $jsonData = json_encode($data);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $headers = [
+        'Host: localhost:2727',
+        'Content-Type: application/json'
+    ];
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+}
+
+?>
+<?php
 require_once 'vendor/autoload.php';
 Twig_Autoloader::register();
+
+$jsonAccess = json_decode($result);
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
     'auto_reload' => true
 ));
 $template = $twig->loadTemplate('Access.html.twig');
-echo $template->render(array('title' => 'CardList'));
+echo $template->render(array('title' => 'AccessList', 'text' => $jsonAccess));
+?>
--- index.php
+++ index.php
@@ -6,17 +6,40 @@ $result = curl_exec($curl);
 curl_close($curl);
 ?>
 
+<?php
+if ($_POST['x'] != ''){
+    echo ' dabsjdfskkndfds';
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $value = $_POST['x'];
+    curl_setopt($curl, CURLOPT_URL, "http://localhost:2727/WcfService.svc/GetLogByDoor/{$value}");
+    $resultbydoor = curl_exec($curl);
+    curl_close($curl);
+}
+?>
+<?php
+if ($_POST['z'] != ''){
+    echo ' dabsjdfskkndfds';
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $value = $_POST['z'];
+    curl_setopt($curl, CURLOPT_URL, "http://localhost:2727/WcfService.svc/GetLogByCard/{$value}");
+    $resultbydoor = curl_exec($curl);
+    curl_close($curl);
+}
+?>
 
 <?php
 require_once 'vendor/autoload.php';
 Twig_Autoloader::register();
 
 $jsonLog = json_decode($result);
+$jsonLogByDoor = json_decode($resultbydoor);
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
     'auto_reload' => true
 ));
 $template = $twig->loadTemplate('index.html.twig');
-echo $template->render(array('title' => 'LogList', 'text' => $jsonLog));
+echo $template->render(array('title' => 'LogList', 'text' => $jsonLog, 'door' => $jsonLogByDoor));
 
